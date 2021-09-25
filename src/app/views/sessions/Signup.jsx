7a8eba33906d668da2f15de08be5df0dd2de87df
Maximiliano Vilchez Chiroque <max.vilchez@outlook.com>
2021-09-24 22:37:45 -0500

    vista de registro cliente y consultor

--- src/app/views/sessions/Signup.jsx
+++ src/app/views/sessions/Signup.jsx
@@ -10,12 +10,27 @@ class Signup extends Component {
     email: "",
     username: "",
     password: "",
-    repassword: ""
+    repassword: "",
+    category: "",
+    isClient: true
   };
+
+  componentDidMount() {
+    this.validateUser();
+  }
+
+  validateUser = () => {
+    if (window.location.href.indexOf('client') === -1)
+      this.setState({ isClient: false })
+  }
 
   handleSubmit = (values, { setSubmitting }) => {
     console.log(values);
   };
+
+  handleChange(event, name) {
+    this.setState({ [name]: event.target.value });
+  }
 
   render() {
     return (
@@ -37,7 +52,7 @@ class Signup extends Component {
               >
                 <div className="pl-3 auth-right">
                   <div className="auth-logo text-center mt-4">
-                    <img src="assets/images/logo.png" alt="" />
+                    <img src="/assets/images/logo.png" alt="" />
                   </div>
                   <div className="flex-grow-1"></div>
                   <div className="w-100 mb-4">
@@ -45,14 +60,14 @@ class Signup extends Component {
                       to="/session/signin"
                       className="btn btn-rounded btn-outline-primary btn-outline-email btn-block btn-icon-text"
                     >
-                      <i className="i-Mail-with-At-Sign"></i> Sign in with Email
+                      <i className="i-Mail-with-At-Sign"></i> Iniciar Sesión
                     </Link>
 
                     <Button className="btn btn-outline-google btn-block btn-icon-text btn-rounded">
-                      <i className="i-Google-Plus"></i> Sign in with Google
+                      <i className="i-Google-Plus"></i> Iniciar sesión con Google
                     </Button>
                     <Button className="btn btn-outline-facebook btn-block btn-icon-text btn-rounded">
-                      <i className="i-Facebook-2"></i> Sign in with Facebook
+                      <i className="i-Facebook-2"></i> Iniciar sesión con Facebook
                     </Button>
                   </div>
                   <div className="flex-grow-1"></div>
@@ -61,10 +76,11 @@ class Signup extends Component {
 
               <div className="col-md-6">
                 <div className="p-4">
-                  <h1 className="mb-3 text-18">Sign Up</h1>
+                  <p className="mb-0 text-18">Registrate como</p>
+                  <h1 className="mb-3 text-bold">{this.state.isClient ? 'Cliente' : 'Consultor'}</h1>
                   <Formik
                     initialValues={this.state}
-                    validationSchema={SignupSchema}
+                    validationSchema={this.state.isClient ? SignupSchemaClient : SignupSchemaConsultant}
                     onSubmit={this.handleSubmit}
                   >
                     {({
@@ -78,7 +94,7 @@ class Signup extends Component {
                     }) => (
                       <form onSubmit={handleSubmit}>
                         <div className="form-group">
-                          <label htmlFor="username">Your name</label>
+                          <label htmlFor="username">Tu nombre</label>
                           <input
                             className="form-control form-control-rounded"
                             name="username"
@@ -94,7 +110,7 @@ class Signup extends Component {
                           )}
                         </div>
                         <div className="form-group">
-                          <label htmlFor="email">Email address</label>
+                          <label htmlFor="email">Email</label>
                           <input
                             name="email"
                             className="form-control form-control-rounded"
@@ -110,7 +126,7 @@ class Signup extends Component {
                           )}
                         </div>
                         <div className="form-group">
-                          <label htmlFor="password">Password</label>
+                          <label htmlFor="password">Contraseña</label>
                           <input
                             name="password"
                             className="form-control form-control-rounded"
@@ -126,7 +142,7 @@ class Signup extends Component {
                           )}
                         </div>
                         <div className="form-group">
-                          <label htmlFor="repassword">Retype password</label>
+                          <label htmlFor="repassword">Repetir contraseña</label>
                           <input
                             name="repassword"
                             className="form-control form-control-rounded"
@@ -141,11 +157,35 @@ class Signup extends Component {
                             </div>
                           )}
                         </div>
+
+                        {!this.state.isClient ?
+                          (<div className="form-group">
+                            <label htmlFor="category">Especialidad</label>
+                            <select
+                              className="form-control form-control-rounded"
+                              name="category"
+                              onChange={(e) => { handleChange(e); this.handleChange(e, "category") }}
+                              onBlur={handleBlur}
+                            >
+                              <option value="">Seleccionar especialidad</option>
+                              <option value="Medicina">Medicina</option>
+                              <option value="Leyes">Leyes</option>
+                              <option value="Agricultura">Agricultura</option>
+                              <option value="Seguridad informática">Seguridad informática</option>
+                            </select>
+                            {errors.category && touched.category && (
+                              <div className="text-danger mt-1 ml-2">
+                                {errors.category}
+                              </div>
+                            )}
+                          </div>) : null
+                        }
+
                         <button
                           className="btn btn-primary btn-block btn-rounded mt-3"
                           type="submit"
                         >
-                          Sign Up
+                          Registrarme
                         </button>
                       </form>
                     )}
@@ -160,20 +200,39 @@ class Signup extends Component {
   }
 }
 
-const SignupSchema = yup.object().shape({
-  username: yup.string().required("email is required"),
+const SignupSchemaConsultant = yup.object().shape({
+  username: yup.string().required("El nombre es requerido"),
   email: yup
     .string()
-    .email("Invalid email")
-    .required("email is required"),
+    .email("Email es invalido")
+    .required("Email es requerido"),
   password: yup
     .string()
-    .min(8, "Password must be 8 character long")
-    .required("password is required"),
+    .min(2, "La contraseña debe tener 8 caracteres")
+    .required("Contraseña es requerido"),
   repassword: yup
     .string()
-    .required("repeat password")
-    .oneOf([yup.ref("password")], "Passwords must match")
+    .required("Repetir contraseña es requerido")
+    .oneOf([yup.ref("password")], "Las contraseñas deben coincidir"),
+  category: yup.string().required("La especialidad es requerida"),
+});
+
+//this.state. .state.isClient? yup.string().nullable : 
+
+const SignupSchemaClient = yup.object().shape({
+  username: yup.string().required("El nombre es requerido"),
+  email: yup
+    .string()
+    .email("Email es invalido")
+    .required("Email es requerido"),
+  password: yup
+    .string()
+    .min(2, "La contraseña debe tener 8 caracteres")
+    .required("Contraseña es requerido"),
+  repassword: yup
+    .string()
+    .required("Repetir contraseña es requerido")
+    .oneOf([yup.ref("password")], "Las contraseñas deben coincidir")
 });
 
 const mapStateToProps = state => ({
